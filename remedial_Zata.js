d4ee71fbd3d67ec06bd9db33687c032c998491d4
dira <hadikirazata@gmail.com>
2022-10-27 13:39:20 +0700

    27_10_2022_REMEDIALzata

---
+++ remedial_Zata.js
@@ -0,0 +1,82 @@
+//assignment
+console.log('=============INCREMENT+++++++++++++++')
+//post increment
+var u = 0
+var t = 10
+var u = b+=4
+console.log(u)
+console.log(t)
+
+//pre increment 
+var a = 0
+var b = 10
+var a = a+=5+b
+console.log(a)
+console.log(b)
+
+console.log('=============DECREMENT+++++++++++++++')
+//pre increment
+var a = 0
+var c = 10
+var a = c-=3
+console.log(a)
+console.log(b)
+
+//pre increment
+var a = 0
+var y = 10
+var a = a-=3-y
+console.log(a)
+console.log(y)
+
+//comparisson
+var num = 10;
+console.log(num == 8);//outputs false
+
+//if 
+var x=1;
+if (x<=5) {
+    console.log("ok")
+}
+
+//if else
+var x=6;
+if (x<=5) {
+    console.log("ok")
+}
+else {
+    console.log("not ok")
+}
+
+//switch case
+var x=1;
+switch(x){
+    case 1:
+        console.log("ok")
+    break;
+    case 2:
+        console.log(3)
+    break;
+    default:
+        console.log("not ok")
+}
+
+//for
+for (i=1; i<=5; i++){
+    console.log("Zata");
+}console.log('\n')
+
+//while
+var H = 1
+while(H<=5){
+    console.log("Dira")
+    H+=1
+}console.log('\n')
+
+//do while
+var R = 1
+do{
+    console.log("Rawrrr")
+    R+=1
+} while(R<=5)
+
